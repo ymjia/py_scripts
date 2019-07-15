216226a160c5b9f358a359daedd0566b8deab2cf
ymjia <ymjia90@163.com>
2019-07-15 20:36:33 +0800

    add signal/slot file

---
+++ test_framework/ui_logic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+## @file ui_logic.py
+## @brief ui signal/slot logitcs
+## @author jiayanming
+
+import os.path
+import math
+import sys
+import datetime
+import xml.etree.ElementTree as ET
+
+from . import project_io
+
+p = project_io.Project()
+
+
+
