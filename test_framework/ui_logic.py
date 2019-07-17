e3728e2eb501fc82d5e7a406aa410e6e8228e6b2
ymjia <ymjia90@163.com>
2019-07-17 15:12:58 +0800

    change logic slot parameters

--- test_framework/ui_logic.py
+++ test_framework/ui_logic.py
@@ -16,7 +16,9 @@ from test_framework.project_io import get_checked_items
 from test_framework.generate_docx import generate_docx
 
 
-def slot_generate_docx(p_obj):
+def slot_generate_docx(ui):
+    ui._p = ui.collect_ui_info()
+    p_obj = ui._p
     dir_i = p_obj._dirInput
     dir_o = p_obj._dirOutput
     dir_doc = os.path.join(dir_o, "doc")
@@ -27,12 +29,13 @@ def slot_generate_docx(p_obj):
         os.makedirs(dir_doc)
     file_save = os.path.join(dir_doc, p_obj._curDocName)
     l_case = get_checked_items(p_obj._case, p_obj._dCaseCheck)
-    l_alg = get_checked_items(p_obj._alg, p_obj._dAlgCheck)
     l_ver = get_checked_items(p_obj._ver, p_obj._dVerCheck)
-    generate_docx(dir_i, dir_o, file_save, l_case, l_alg, l_ver)
+    l_alg = get_checked_items(p_obj._alg, p_obj._dAlgCheck)
+    generate_docx(p_obj._configFile, dir_i, dir_o, file_save, l_case, l_ver, l_alg)
 
 
-def slot_open_docx(p_obj):
+def slot_open_docx(ui):
+    p_obj = ui._p
     dir_doc = os.path.join(p_obj._dirOutput, "doc")
     file_doc = os.path.join(dir_doc, p_obj._curDocName)
     if os.path.exists(file_doc):
@@ -41,14 +44,16 @@ def slot_open_docx(p_obj):
         QMessageBox.about(None, "Error", "Document doesnot Exist! Try Generate Docx First.")
 
 
-def slot_open_docx_path(p_obj):
+def slot_open_docx_path(ui):
+    p_obj = ui._p
     dir_doc = os.path.join(p_obj._dirOutput, "doc")
     if not os.path.exists(dir_doc):
         os.makedirs(dir_doc)
     os.startfile(dir_doc)
 
 
-def slot_create_screenshots(p_obj):
+def slot_create_screenshots(ui):
+    p_obj = ui._p
     dir_i = p_obj._dirInput
     dir_o = p_obj._dirOutput
     l_case = get_checked_items(p_obj._case, p_obj._sCaseCheck)
