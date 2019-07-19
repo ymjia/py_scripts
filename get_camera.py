485027e0336ece8322dc53e71d7463b78b5bf240
ymjia <ymjia90@163.com>
2019-07-19 09:49:31 +0800

    (pv_macro)get camera write to config file

--- get_camera.py
+++ get_camera.py
@@ -2,8 +2,14 @@
 ## @file get_camera.py
 ## @brief record camera position of current view
 ## @author jiayanming
+import os
+import sys
 from tkinter import Tk
 from paraview.simple import *
+dir_pv_module = os.path.join(os.getcwd(), "..", "SN3D_plugins", "scripts", "pv_module")
+sys.path.append(dir_pv_module)
+from copy_display import get_view_source
+
 
 def fill_list(res, idx, in_list):
     if isinstance(in_list, int) or isinstance(in_list, float):
@@ -26,11 +32,32 @@ idx += fill_list(camera_pos, idx, v.CameraViewUp)
 print(*camera_pos, sep=",")
 
 clip_str = str(camera_pos[0])
-for i in range(1, len(camera_pos)):
-    clip_str += ", {}".format(camera_pos[i])
-r = Tk()
-r.withdraw()
-r.clipboard_clear()
-r.clipboard_append(clip_str)
-r.update()
-r.destroy()
+# for i in range(1, len(camera_pos)):
+#     clip_str += ", {}".format(camera_pos[i])
+# r = Tk()
+# r.withdraw()
+# r.clipboard_clear()
+# r.clipboard_append(clip_str)
+# r.update()
+# r.destroy()
+
+
+def get_source_dir(s):
+    if not hasattr(s, "FileNames"):
+        return ""
+    f_list = s.FileNames
+    if len(f_list) < 1:
+        return ""
+    return os.path.dirname(f_list[0])
+
+
+# change txt config file
+s_list = GetSources().values()
+cur_source = get_view_source(s_list, v)
+s_dir = get_source_dir(cur_source)
+if s_dir != "" and os.path.exists(s_dir):
+    file_config = os.path.join(s_dir, "config.txt")
+    if os.path.exists(file_config):
+        f = open(file_config, "a")
+        f.writelines(clip_str + "\n")
+        f.close()
