fc359f8e08f7c688b5d38479e6cdfa1ccd2bb994
ymjia <ymjia90@163.com>
2019-07-23 09:44:17 +0800

    (logic): time table read alg_name from file, irrelevant to ui

--- test_framework/utils.py
+++ test_framework/utils.py
@@ -32,7 +32,7 @@ def get_table(dir_o, case, ver):
 
 
 # generate xlsx table file
-def get_compare_table(dir_out, l_case, l_ver, l_alg, file_out):
+def get_compare_table(dir_out, l_case, l_ver, file_out):
     wb = Workbook()
     ws = wb.active
     # | alg | case     |    case2   |
@@ -40,7 +40,6 @@ def get_compare_table(dir_out, l_case, l_ver, l_alg, file_out):
     # |alg1 | 0.1 | 0.2|  0.2| 0.3  |
     case_num = len(l_case)
     ver_num = len(l_ver)
-    alg_num = len(l_alg)
     # table title
     title_line = ["Case"]
     ver_line = ["Version"]
@@ -56,19 +55,29 @@ def get_compare_table(dir_out, l_case, l_ver, l_alg, file_out):
         ws.merge_cells(start_row=1, end_row=1,
                        start_column=i * ver_num + 2,
                        end_column=(i+1) * ver_num + 1)
-    for i in range(0, alg_num):
-        ws.cell(row=i + 3, column=1).value = l_alg[i]
     # table data
     d_case = {}
     d_ver = {}
-    d_alg = {}
     for i in range(0, len(l_case)):
         d_case[l_case[i]] = i
     for i in range(0, len(l_ver)):
         d_ver[l_ver[i]] = i
-    for i in range(0, len(l_alg)):
-        d_alg[l_alg[i]] = i
+    # initial quadric item list
+    d_alg = {}
+    idx_alg = 0
+    time_quad = []
+    for case in l_case:
+        for ver in l_ver:
+            t_alg = get_table(dir_out, case, ver)
+            if t_alg is None:
+                continue
+            for alg, t in t_alg.items():
+                if alg not in d_alg:
+                    d_alg[alg] = idx_alg
+                    idx_alg += 1
+                time_quad.append([case, ver, alg, t])
 
+    # get cell position for given case,ver,alg
     def get_pos(case, ver, alg):
         if case not in d_case or ver not in d_ver or alg not in d_alg:
             return -1, -1
@@ -77,19 +86,14 @@ def get_compare_table(dir_out, l_case, l_ver, l_alg, file_out):
         vid = d_ver[ver]
         col = ver_num * cid + vid + 2
         return row, col
+    # fill time data
+    for quad in time_quad:
+        r, c = get_pos(quad[0], quad[1], quad[2])
+        ws.cell(row=r, column=c).value = quad[3]
+    # alg name column
+    for item in d_alg.items():
+        ws.cell(row=item[1] + 3, column=1).value = item[0]
 
-    for case in l_case:
-        for ver in l_ver:
-            t_alg = get_table(dir_out, case, ver)
-            if t_alg is None:
-                continue
-            for alg in l_alg:
-                if alg not in t_alg:
-                    continue
-                r, c = get_pos(case, ver, alg)
-                if r == -1 or c == -1:
-                    continue
-                ws.cell(row=r, column=c).value = t_alg[alg]
     wb.save(file_out)
 
 
@@ -97,6 +101,6 @@ if __name__ == "__main__":
     file_out = "c:/tmp/time.xlsx"
     l_case = ["case1", "case2"]
     l_ver = ["v11", "v12"]
-    l_alg = ["merge", "smooth"]
+    #l_alg = ["merge", "smooth"]
     dir_out = "c:/data/test_framework/management/project1/output/"
-    get_compare_table(dir_out, l_case, l_ver, l_alg, file_out)
+    get_compare_table(dir_out, l_case, l_ver, file_out)
