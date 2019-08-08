84b455086d6a98b9fbd99347f0e7043736630555
ymjia <ymjia90@163.com>
2019-08-08 19:15:28 +0800

    write process/machine information

--- test_framework/thread_module.py
+++ test_framework/thread_module.py
@@ -25,6 +25,7 @@ class ExeRunThread(QThread):
         self._mainWindow = w_main
         self._demoProc = None
         self._fLog = None
+        self._fSts = None
 
     def __del__(self):
         self.wait()
@@ -40,19 +41,26 @@ class ExeRunThread(QThread):
             return
         pg = 95 / len(list_case)
         cur_pg = 5
+        sys_info = utils.get_sys_info()
         for case in list_case:
             self._sigProgress.emit(cur_pg)
             cur_pg += pg
             # write logs
-            dir_log = os.path.join(dir_o, case, cur_ver)
+            dir_log = os.path.join(dir_o, case, cur_ver, "logs")
             if not os.path.exists(dir_log):
                 os.makedirs(dir_log)
             st = str(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
             file_log = os.path.join(dir_log, "tfl_{}.log".format(st))
+            file_sts = os.path.join(dir_log, "tfl_{}.sts".format(st))
             try:
                 self._fLog = open(file_log, "w")
+                self._fSts = open(file_sts, "a")
+                # write machine info
+                for line in sys_info:
+                    self._fSts.write(line)
             except IOError:
                 self._fLog = None
+                self._fSts = None
                 print("Warning! Fail to open log file {}".format(file_log))
             # run demo
             param = ui_logic.generate_exe_param(p_obj, case)
@@ -65,23 +73,31 @@ class ExeRunThread(QThread):
                     for line in self._demoProc.p.stdout:
                         lined = line.decode('utf-8')
                         sys.stdout.write(lined)
-                        self._fLog.write(lined)
+                        if self._fLog is not None:
+                            self._fLog.write(lined)
                     time.sleep(.5)
             finally:
                 self._demoProc.close()
-            print('return code: {}'.format(self._demoProc.p.returncode))
-            print('time: {}'.format(self._demoProc.t1 - self._demoProc.t0))
-            print('max_virtual_memory: {}'.format(self._demoProc.max_vms_memory))
-            print('max_memory: {}'.format(self._demoProc.max_rss_memory))
-            #self._demoProc.wait()
-            if self._fLog is not None:
-                if not self._fLog.closed:
-                    self._fLog.close()
+            if self._fSts is not None:
+                self._fSts.write('Time {}\n'.format(self._demoProc.t1 - self._demoProc.t0))
+                self._fSts.write('Max_p_mem {}MB\n'.format(self._demoProc.max_pmem / 1e6))
+                self._fSts.write('Max_v_mem {}MB\n'.format(self._demoProc.max_vmem / 1e6))
+            self.release_files()
         self._sigProgress.emit(99)
         ver = p_obj._eVer
         if ver != "" and ver not in p_obj._ver:
             p_obj._ver.append(ver)
             self._mainWindow.fill_ui_info(p_obj)
+
+    def release_files(self):
+        if self._fLog is not None:
+            if not self._fLog.closed:
+                self._fLog.close()
+            self._fLog = None
+        if self._fSts is not None:
+            if not self._fSts.closed:
+                self._fSts.close()
+            self._fSts = None
 
     def closeEvent(self):
         self._sigProgress.cancel()
--- test_framework/ui_logic.py
+++ test_framework/ui_logic.py
@@ -282,9 +282,7 @@ def slot_exe_stop(ui):
     if ui._threadExe is not None:
         if ui._threadExe._demoProc is not None:
             ui._threadExe._demoProc.close(kill=True)
-        if ui._threadExe._fLog is not None:
-            if not ui._threadExe._fLog.closed:
-                ui._threadExe._fLog.close()
+        ui._threadExe.release_files()
         ui._threadExe.terminate()
         ui._threadExe.wait()
         QMessageBox.about(ui, "Message", "Demo Terminated!")
