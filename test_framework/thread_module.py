a9764da82e63aba11dcf21466780bb79cae6c145
ymjia <ymjia90@163.com>
2019-08-19 23:45:20 +0800

    add mem/cpu percentange sample

--- test_framework/thread_module.py
+++ test_framework/thread_module.py
@@ -26,6 +26,7 @@ class ExeRunThread(QThread):
         self._demoProc = None
         self._fLog = None
         self._fSts = None
+        self._fSmp = None
 
     def __del__(self):
         self.wait()
@@ -56,15 +57,18 @@ class ExeRunThread(QThread):
             st = str(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
             file_log = os.path.join(dir_log, "tfl_{}.log".format(st))
             file_sts = os.path.join(dir_log, "tfl_{}.sts".format(st))
+            file_smp = os.path.join(dir_log, "tfl_{}.smp".format(st))
             try:
                 self._fLog = open(file_log, "w")
                 self._fSts = open(file_sts, "a")
+                self._fSmp = open(file_smp, "a")
                 # write machine info
                 for line in sys_info:
                     self._fSts.write(line)
             except IOError:
                 self._fLog = None
                 self._fSts = None
+                self._fSmp = None
                 print("Warning! Fail to open log file {}".format(file_log))
             # run demo
             param = ui_logic.generate_exe_param(p_obj, case)
@@ -89,6 +93,9 @@ class ExeRunThread(QThread):
                 self._fSts.write('Time {0:.2f}\n'.format(self._demoProc.t1 - self._demoProc.t0))
                 self._fSts.write('Max_p_mem {0:.2f}MB\n'.format(self._demoProc.max_pmem / 1e6))
                 self._fSts.write('Max_v_mem {0:.2f}MB\n'.format(self._demoProc.max_vmem / 1e6))
+            if self._fSmp is not None:
+                for li in range(0, len(self._demoProc._cpuSample)):
+                    self._fSmp.write("{} {}\n".format(self._demoProc._cpuSample[li], self._demoProc._memSample[li]))
             self.release_files()
         self._sigProgress.emit(99)
         ver = p_obj._eVer
@@ -105,6 +112,10 @@ class ExeRunThread(QThread):
             if not self._fSts.closed:
                 self._fSts.close()
             self._fSts = None
+        if self._fSmp is not None:
+            if not self._fSmp.closed:
+                self._fSmp.close()
+            self._fSmp = None
 
     def closeEvent(self):
         self._sigProgress.cancel()
